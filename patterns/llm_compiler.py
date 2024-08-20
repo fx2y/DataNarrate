e8b790d66d3ed609b2c0250edc0b8b200a4d1d4f
abdullah <44318011+fx2y@users.noreply.github.com>
2024-08-20 21:31:48 +0700

    feat: integrate config, refactor LLM setup, enhance task scheduling and parsing

--- patterns/llm_compiler.py
+++ patterns/llm_compiler.py
@@ -1,12 +1,12 @@
 import getpass
 import itertools
+import json
 import logging
 import os
 import re
 from concurrent.futures import ThreadPoolExecutor, wait
-from typing import Sequence, List, Dict, Any, Union, Iterable, Annotated
+from typing import Sequence, List, Dict, Any, Union, Iterable, Annotated, Optional
 
-from langchain import hub
 from langchain.chains.openai_functions import create_structured_output_runnable
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.language_models import BaseChatModel
@@ -20,12 +20,20 @@ from langgraph.graph import END, StateGraph, START
 from langgraph.graph.message import add_messages
 from typing_extensions import TypedDict
 
+from datanarrate.config import config
 from math_tools import get_math_tool
 from output_parser import LLMCompilerPlanParser, Task
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+llm = ChatOpenAI(
+    model_name=config.LLM_MODEL_NAME,
+    openai_api_base=config.OPENAI_API_BASE,
+    openai_api_key=config.OPENAI_API_KEY,
+    temperature=0.2
+)
 
 
 # Helper function to get environment variables
@@ -36,9 +44,9 @@ def _set_if_undefined(var: str):
 
 # Set up environment variables for tracing
 def setup_environment():
-    os.environ["LANGCHAIN_TRACING_V2"] = "True"
-    os.environ["LANGCHAIN_PROJECT"] = "LLMCompiler"
-    _set_if_undefined("LANGCHAIN_API_KEY")
+    # os.environ["LANGCHAIN_TRACING_V2"] = "True"
+    # os.environ["LANGCHAIN_PROJECT"] = "LLMCompiler"
+    # _set_if_undefined("LANGCHAIN_API_KEY")
     _set_if_undefined("OPENAI_API_KEY")
     _set_if_undefined("TAVILY_API_KEY")
 
@@ -46,7 +54,7 @@ def setup_environment():
 setup_environment()
 
 # Define tools
-calculate = get_math_tool(ChatOpenAI(model="gpt-4-turbo-preview"))
+calculate = get_math_tool(llm)
 search = TavilySearchResults(
     max_results=1,
     description='tavily_search_results_json(query="the search query") - a search engine.',
@@ -180,9 +188,31 @@ def _resolve_arg(arg: Union[str, Any], observations: Dict[int, Any]):
         idx = int(match.group(1))
         return str(observations.get(idx, match.group(0)))
 
+    def parse_custom_string(s: str):
+        # Convert Python-style dict to JSON
+        def replace_quotes(match):
+            return match.group(0).replace("'", '"')
+
+        s = re.sub(r"'(\w+)':", r'"\1":', s)  # Replace quotes around keys
+        s = re.sub(r":\s*'([^']*)'", replace_quotes, s)  # Replace quotes around values
+
+        try:
+            # Parse as JSON
+            return json.loads(s)
+        except json.JSONDecodeError:
+            # If JSON parsing fails, return the original string
+            return s
+
+    # For dependencies on other tasks
     if isinstance(arg, str):
+        # resolved = re.sub(ID_PATTERN, replace_match, arg)
+        # if resolved.startswith("[") and resolved.endswith("]") or resolved.startswith("{") and resolved.endswith("}"):
+        #     return parse_custom_string(resolved)
+        # return resolved
         return re.sub(ID_PATTERN, replace_match, arg)
     elif isinstance(arg, list):
+        # if len(arg) == 1:
+        #     return _resolve_arg(arg[0], observations)
         return [_resolve_arg(a, observations) for a in arg]
     else:
         return str(arg)
@@ -240,13 +270,34 @@ def schedule_tasks(scheduler_input: SchedulerInput) -> List[FunctionMessage]:
             futures.append(executor.submit(schedule_pending_task, task, observations))
         wait(futures)
     new_observations = {
-        k: FunctionMessage(name=f"task_{k}", content=v) for k, v in observations.items()
+        k: FunctionMessage(name=f"task_{k}", content=str(v)) for k, v in observations.items()
     }
     return list(new_observations.values())
 
 
-llm = ChatOpenAI(model="gpt-4-turbo-preview")
-prompt = hub.pull("wfh/llm-compiler")
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """Given a user query, create a plan to solve it with the utmost parallelizability. Each plan should comprise an action from the following {num_tools} types:
+{tool_descriptions}
+{num_tools}. join(): Collects and combines results from prior actions.
+
+ - An LLM agent is called upon invoking join() to either finalize the user query or wait until the plans are executed.
+ - join should always be the last action in the plan, and will be called in two scenarios:
+   (a) if the answer can be determined by gathering the outputs from tasks to generate the final response.
+   (b) if the answer cannot be determined in the planning phase before you execute the plans. Guidelines:
+ - Each action described above contains input/output types and description.
+    - You must strictly adhere to the input and output types for each action.
+    - The action descriptions contain the guidelines. You MUST strictly follow those guidelines when you use the actions.
+ - Each action in the plan should strictly be one of the above types. Follow the Python conventions for each action.
+ - Each action MUST have a unique ID, which is strictly increasing.
+ - Inputs for actions can either be constants or outputs from preceding actions. In the latter case, use the format $id to denote the ID of the previous action whose output will be the input.
+ - Always call join as the last action in the plan. Say '<END_OF_PLAN>' after you call join
+ - Ensure the plan maximizes parallelizability.
+ - Only use the provided action types. If a query cannot be addressed using these, invoke the join action for the next steps.
+ - Never introduce new actions other than the ones provided."""),
+    ("placeholder", "{messages}"),
+    ("system", """Remember, ONLY respond with the task list in the correct format! E.g.:
+idx. tool(arg_name=args)"""),
+])
 planner = create_planner(llm, tools, prompt)
 
 
@@ -266,7 +317,7 @@ def plan_and_schedule(state):
             "tasks": tasks,
         }
     )
-    return {"messages": [scheduled_tasks]}
+    return {"messages": scheduled_tasks}
 
 
 # Joiner
@@ -284,11 +335,28 @@ class JoinOutputs(BaseModel):
     action: Union[FinalResponse, Replan]
 
 
-joiner_prompt = hub.pull("wfh/llm-compiler-joiner").partial(examples="")
+joiner_prompt = ChatPromptTemplate.from_messages([
+    ("system", """Solve a question answering task. Here are some guidelines:
+ - In the Assistant Scratchpad, you will be given results of a plan you have executed to answer the user's question.
+ - Thought needs to reason about the question based on the Observations in 1-2 sentences.
+ - Ignore irrelevant action results.
+ - If the required information is present, give a concise but complete and helpful answer to the user's question.
+ - If you are unable to give a satisfactory finishing answer, replan to get the required information. Respond in the following format:
+
+Thought: <reason about the task results and whether you have sufficient information to answer the question>
+Action: <action to take>
+Available actions:
+ (1) Finish(the final answer to return to the user): returns the answer and finishes the task.
+ (2) Replan(the reasoning and other information that will help you plan again. Can be a line of any length): instructs why we must replan"""),
+    ("placeholder", "{messages}"),
+    ("system", """Using the above previous actions, decide whether to replan or finish. If all the required information is present. You may finish. If you have made many attempts to find the information without success, admit so and respond with whatever information you have gathered so the user can work well with you.
+
+{examples}"""),
+]).partial(examples="")
 runnable = create_structured_output_runnable(JoinOutputs, llm, joiner_prompt)
 
 
-def _parse_joiner_output(decision: JoinOutputs) -> List[BaseMessage]:
+def _parse_joiner_output(decision: JoinOutputs) -> Dict[str, Any]:
     """Parse the output from the joiner.
 
     Args:
@@ -299,10 +367,10 @@ def _parse_joiner_output(decision: JoinOutputs) -> List[BaseMessage]:
     """
     response = [AIMessage(content=f"Thought: {decision.thought}")]
     if isinstance(decision.action, Replan):
-        response.append(SystemMessage(content=decision.action.feedback))
+        response.append(AIMessage(content=decision.action.feedback))
     else:
         response.append(AIMessage(content=decision.action.response))
-    return response
+    return {"messages": response, "decision": decision}
 
 
 def select_recent_messages(state) -> dict:
@@ -330,6 +398,7 @@ joiner = select_recent_messages | runnable | _parse_joiner_output
 # Compose using LangGraph
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+    decision: Optional[JoinOutputs]
 
 
 graph_builder = StateGraph(State)
@@ -347,18 +416,19 @@ def should_continue(state):
     Returns:
         str: The next step in the process.
     """
-    messages = state["messages"]
-    if isinstance(messages[-1], AIMessage):
+    decision = state["decision"]
+    if isinstance(decision.action, Replan):
         return "plan_and_schedule"
     return END
 
 
-graph_builder.add_conditional_edges(start_key="join", condition=should_continue)
+graph_builder.add_conditional_edges("join", should_continue)
 graph_builder.add_edge(START, "plan_and_schedule")
 chain = graph_builder.compile()
 
-# Example usage
-example_question = "What's the temperature in SF raised to the 3rd power?"
-for step in chain.stream({"messages": [HumanMessage(content=example_question)]}):
-    print(step)
-    print("---")
+if __name__ == '__main__':
+    # Example usage
+    example_question = "What's the temperature in SF raised to the 3rd power?"
+    for step in chain.stream({"messages": [HumanMessage(content=example_question)]}):
+        print(step)
+        print("---")
